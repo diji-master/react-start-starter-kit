c4011a43cb9cba980942567d6686b0f44958d541
diji-master <dijitrendmastery@gmail.com>
2023-06-12 08:42:14 +0300

    Create env file for product & development

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import logo from "./logo.svg";
 function App() {
   return (
     <div className="App">
-      <img src="/logo192.png" alt="Logo" />
-      <img src={logo} alt="Logo" />
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>{process.env.REACT_APP_API_URL}</p>
+      {process.env.NODE_ENV === "development" && (
+        <div>
+          <img src="/logo192.png" alt="Logo" />
+          <img src={logo} alt="Logo" />
+        </div>
+      )}
     </div>
   );
 }
